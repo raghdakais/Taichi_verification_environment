8056b0345e1155225a8f71e4c385798f2e1e5889
Kais <partner.Raghda.Kais@philips.com>
2025-03-22 01:10:48 +0200

    adding RO test for diagnostic and fixing tcl file

--- sim/run_sim.tcl
+++ sim/run_sim.tcl
@@ -1,28 +1,15 @@
-# Disable transcript output to clean the console
 transcript off
-
-# Set warning for numeric standard no warnings
 set NumericStdNoWarnings 1
 
-# Set project, log, and coverage paths
-set PROJ_PATH "../"         # Path to the project directory
-set LOG_PATH "../log_result" # Path to store log results
-set COV_PATH "../coverage"  # Path to store coverage results
+set PROJ_PATH "../"
+set LOG_PATH "../log_result"
+set COV_PATH "../coverage"
 
 # Default coverage flag (0 = disabled)
 if {![info exists enable_cov]} {
-    set enable_cov 0   # Set coverage flag to 0 if not already set
+    set enable_cov 0
 }
 
-# Default test name (can be overridden by input argument)
-set test_name "diagnostic_registers_random_test"
-
-# Check if a test name argument is provided from the command line
-if {[llength $argv] > 0} {
-    set test_name [lindex $argv 0]  # Override the default test name with the first argument
-}
-
-puts "Running test: $test_name"
 
 # Check if LOG_PATH exists, if not, create it
 if {![file exists $LOG_PATH]} {
@@ -35,11 +22,11 @@ if {![file exists $COV_PATH]} {
     puts "COV_PATH does not exist, creating directory: $COV_PATH"
     file mkdir $COV_PATH
 }
-
 # Get the current date and time in YYYY-MM-DD_HH-MM-SS format
 set current_datetime [clock format [clock seconds] -format "%Y_%m_%d_%H_%M_%S"]
 
 # Define the base folder path inside LOG_PATH using the current date and time
+####set folder_path "${LOG_PATH}/work_${current_datetime}"
 set folder_path "${LOG_PATH}/."
 
 # Define the full work library path inside the folder
@@ -54,22 +41,24 @@ vlib $work_library_path
 # Map the "work" library to its path
 vmap work $work_library_path
 
-# Map secureip and unisim libraries to their paths (for simulation)
+# Map secureip and unisim libraries to their paths
 vmap fifo_generator_v13_2_7 C:/questasim64_2024.1/fifo_generator_v13_2_7
 vmap secureip C:/questasim64_2024.1/secureip
 vmap unisim C:/questasim64_2024.1/unisim
 vmap xpm C:/questasim64_2024.1/xpm
 
-# Output confirmation of library creation
+ 
+
+
+# Output confirmation
 puts "Created and mapped work library at: $work_library_path"
 
-# Check if compile environment scripts exist
+# Compile design
 if { ![file exists compile_env.do] || ![file exists compile_design.do] } {
     puts "Error: Missing compile scripts!"
-    exit 1  # Exit if compile scripts are missing
+    exit 1
 }
 
-# Compile the design and environment
 puts "Compiling Design..."
 do compile_design.do
 puts "Compiling Environment..."
@@ -81,35 +70,40 @@ vdir
 # Open waveform and add signals (optional)
 view wave
 # Uncomment the line below to add all signals to the waveform
-# add wave -r /*
+#add wave -r /*
 
-# Enable coverage collection if the flag is set
+
+
+# Enable coverage if the flag is set
 if {$enable_cov} {
     puts "Coverage collection enabled."
     set COVERAGE_DB "${COV_PATH}/functional_coverage.ucdb"
-    set vopt_args "+cover=bcesft"   # Set coverage options
-    set vsim_args "-coverage"       # Set simulation arguments for coverage
+    set vopt_args "+cover=bcesft"
+    set vsim_args "-coverage"
 } else {
     puts "Coverage collection disabled."
-    set vopt_args ""    # No coverage arguments if disabled
-    set vsim_args ""    # No coverage in simulation if disabled
+    set vopt_args ""
+    set vsim_args ""
 }
 
+
 # Optimize the design with vopt
-puts "Start Optimization.."
-vopt +acc work.taichi_tmb_tb -o taichi_tmb_optimized_sim $vopt_args
+# +cover=bcst  -- to enable coverage
+ puts "Start Optimization.."
+vopt +acc  work.taichi_tmb_tb -o taichi_tmb_optimized_sim $vopt_args
+
 
 # Run the optimized simulation
-puts "Start VSIM command.."
+ puts "Start VSIM command.."
 vsim -L unisim -L secureip -L fifo_generator_v13_2_7 -L xpm \
      +UVM_VERBOSITY=UVM_LOW -c -l "${LOG_PATH}/transcript" \
      -cvgperinstance -vopt -voptargs=+acc -coverage -sva -c work.taichi_tmb_optimized_sim \
-     +UVM_TESTNAME=$test_name -onfinish stop  -do "set StdArithNoWarnings 1 ; set NumericStdNoWarnings 1"
+     +UVM_TESTNAME=diagnostic_check_default_test -onfinish stop  -do "set StdArithNoWarnings 1 ; set NumericStdNoWarnings 1"
 
-# Output simulation running status
+
 puts "Running simulation..."
 
-# Save all signals to a log
+# Save all signals
 log -r /*
 
 # Run simulation for a fixed time (e.g., 10,000 time units)
@@ -117,10 +111,12 @@ if {[catch {run -all} err]} {
     puts "Error: $err"
 }
 
+
 # Save coverage results if enabled
 if {$enable_cov} {
-    coverage save ${COV_PATH}/functional_coverage_result.ucdb
-    xml2ucdb -format Excel testplan.xml -ucdbfilename ${COV_PATH}/testplan.ucdb
-    vcover merge testplan.ucdb ${COV_PATH}/testplan.ucdb ${COV_PATH}/functional_coverage_result.ucdb 
-    vcover report -cvg -details -nocompactcrossbins -codeAll -assert -directive -html -htmldir ${COV_PATH}/htmlcoverreport testplan.ucdb -details -testhitdata
+coverage save  ${COV_PATH}/functional_coverage_result.ucdb
+xml2ucdb -format Excel testplan.xml -ucdbfilename ${COV_PATH}/testplan.ucdb
+vcover merge testplan.ucdb ${COV_PATH}/testplan.ucdb ${COV_PATH}/functional_coverage_result.ucdb 
+vcover report -cvg -details -nocompactcrossbins -codeAll -assert -directive -html -htmldir ${COV_PATH}/htmlcoverreport testplan.ucdb -details -testhitdata
 }
+
---
+++ tests/diagnostic_check_default_test.sv
@@ -0,0 +1,51 @@
+// UVM test base for taichi_tmb environment
+//===============================================================================
+class diagnostic_check_default_test extends taichi_tmb_test_base;
+//===============================================================================
+    // Testbench configuration and stimulus generation
+       `uvm_component_utils(diagnostic_check_default_test)  // Register with the factory
+  
+
+        // This is standard code for all components
+    function new (string name = "diagnostic_check_default_test", uvm_component parent = null);
+      super.new (name, parent);
+    endfunction
+
+
+//===============================================================================
+    function void build_phase(uvm_phase phase);
+//===============================================================================
+        super.build_phase(phase);
+    endfunction
+
+    
+//===============================================================================
+    task run_phase (uvm_phase phase);
+//===============================================================================
+
+    	phase.raise_objection (this);
+         	super.run_phase(phase);
+
+    //------------------------------------------------------
+    // Check Default Value
+    //------------------------------------------------------
+    foreach(DIAG_REGISTERS[i])
+     begin
+     //    m_diag_txrx_seq = TXRX_sequence::type_id::create ("m_diag_txrx_seq_%d",i);
+         m_diag_txrx_seq = TXRX_sequence::type_id::create($sformatf("m_diag_txrx_seq_%0d", i));
+             if (!this.m_diag_txrx_seq.randomize() with { 
+                m_diag_txrx_seq.item.rw_type == TXRX_READ;    
+                m_diag_txrx_seq.item.address == DIAG_REGISTERS[i].address;    
+                 }) 
+              `uvm_fatal("RUN_PHASE", "Randomization failed for m_diag_txrx_seq")
+             this.m_diag_txrx_seq.start(this.m_taichi_tmb_env.diag_txrx_agent.seqr);
+     #1.5us;
+      end
+  
+
+
+    #100us;
+    	phase.drop_objection (this);
+   endtask
+
+endclass
